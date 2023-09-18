fdaf0f780c0a7ac4b61c4c5c10a8c4bfdefc6f5f
FixxxarN <tornstromandre@gmail.com>
2023-09-18 12:39:15 +0200

    Made timeline mobile friendly and added more text

--- src/components/Body/TimelineSection/Timeline/index.js
+++ src/components/Body/TimelineSection/Timeline/index.js
@@ -2,19 +2,24 @@ import React from 'react';
 import { Box, Typography } from '@mui/material';
 import { List, ListItem, TimelineWrapper } from './styles';
 import { EXPERIENCE_DATA } from './data';
+import useIsMobile from '../../../../hooks/useIsMobile';
 
 const Timeline = () => {
+  const isMobile = useIsMobile();
 
   return (
     <Box display="flex" flex={1} alignItems="center" justifyContent="center" bgcolor="#000" color="#fff">
-      <TimelineWrapper>
-        <List>
+      <TimelineWrapper isMobile={isMobile}>
+        <List isMobile={isMobile}>
           {
             EXPERIENCE_DATA.map((experience) => (
-              <ListItem key={experience.company}>
+              <ListItem key={experience.company} isMobile={isMobile}>
                 <Box>
                   <h2>{experience.company}</h2>
+                  <p>{experience.title}</p>
+                  <p>{experience.text}</p>
                   <p>{experience.interval}</p>
+                  <p>{experience.place}</p>
                 </Box>
               </ListItem>
             ))
--- src/components/Body/TimelineSection/Timeline/styles.js
+++ src/components/Body/TimelineSection/Timeline/styles.js
@@ -2,21 +2,22 @@ import styled from "@emotion/styled";
 import { Box } from "@mui/material";
 
 export const TimelineWrapper = styled(Box)`
-  width: 80%; 
   height: auto; 
   margin: 0 auto;
   max-width: 800px;
   position: relative;
 
-  &&::before {
-    content: '';
-    position: absolute;
-    height: 100%;
-    width: 2px;
-    left: 50%;
-    transform: translateX(-50%);
-    background-color: #fff;
-  }
+  ${({ isMobile }) => !isMobile && `
+    &&::before {
+      content: '';
+      position: absolute;
+      height: 100%;
+      width: 2px;
+      left: 50%;
+      transform: translateX(-50%);
+      background-color: #fff;
+    }
+  `}
 `;
 
 export const List = styled.ul`
@@ -24,50 +25,60 @@ export const List = styled.ul`
   padding-left: 0px;
   margin-top: 0px;
 
-  && li:last-child {
-    margin-bottom: 0px;
-  }
+  ${({ isMobile }) => !isMobile && `
+    && li:last-child {
+      margin-bottom: 0px;
+    }
 
-  && li:nth-child(odd) {
-    float: left;
-    clear: right;
-    transform: translateX(-60px);
-  }
+    && li:nth-child(odd) {
+      float: left;
+      clear: right;
+      transform: translateX(-60px);
+    }
 
-  && li:nth-child(even) {
-    float: right;
-    clear: left;
-    transform: translateX(60px);
-  }
+    && li:nth-child(even) {
+      float: right;
+      clear: left;
+      transform: translateX(60px);
+    }
 
-  && li::after {
-    content: '';
-    position: absolute;
-    height: 20px;
-    width: 20px;
-    background: #fff;
-    border-radius: 20px;
-    top: 0px;
-  }
+    && li::after {
+      content: '';
+      position: absolute;
+      height: 20px;
+      width: 20px;
+      background: #fff;
+      border-radius: 20px;
+      top: 0px;
+    }
 
-  && li:nth-child(odd)::after {
-    transform: translate(50%, -50%);
-    right: -20px;
-  }
+    && li:nth-child(odd)::after {
+      transform: translate(50%, -50%);
+      right: -20px;
+    }
 
-  && li:nth-child(even)::after {
-    transform: translate(-50%, -50%);
-    left: -20px;
-  }
+    && li:nth-child(even)::after {
+      transform: translate(-50%, -50%);
+      left: -20px;
+    }
+  `}
 `;
 
 export const ListItem = styled.li`
-  width: 50%;
+  ${({ isMobile }) => !isMobile ? `
+    width: 50%;
+    margin-bottom: 100px;
+  ` : `
+    flex: 1;
+    margin-left: 10px;
+    margin-right: 10px;
+    margin-bottom: 50px;
+  `}
+
   position: relative;
 
   padding: 20px;
   background: #101010;
   color: #fff;
   border-radius: 10px;
-  margin-bottom: 100px;
 `;
